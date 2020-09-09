e216cfe84021f8de79c82500b932a0be2f648921
Tim McIver <tim@timmciver.com>
2020-09-08 22:09:15 -0400

    Initial commit.
    Making app public, creating authorizations and creating container.

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage:   $0 $base $cert_pem_file $cert_password" >&2
+  echo "Example: $0" 'https://localhost:4443/ ../../../LinkedDataHub/certs/owner.p12.pem Password' >&2
+  echo "Note: special characters such as $ need to be escaped in passwords!" >&2
+  exit 1
+fi
+
+base="$1"
+cert_pem_file=$(realpath -s "$2")
+cert_password="$3"
+pwd="$(realpath -s "$PWD")"
+
+pushd . && cd ./admin
+
+printf "\n### Making the app public\n\n"
+
+./make-public.sh "$base" "$cert_pem_file" "$cert_password"
+
+popd
+
+printf "\n### Creating authorizations\n\n"
+
+find "${pwd}" -name '*.ttl' -exec ./admin/acl/create-authorization.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} \;
+
+printf "\n### Create containers\n\n"
+
+./create-containers.sh "$base" "$cert_pem_file" "$cert_password"
